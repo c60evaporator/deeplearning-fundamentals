2e82bfff48d7554b3c67c461fb56e294aeb9a89d
knakamura <c60evaporator@gmail.com>
2023-02-06 02:44:30 -0800

    Small modification

--- common/utils.py
+++ common/utils.py
@@ -19,14 +19,17 @@ def calc_weight_init_std(prev_node_num, weight_init_std, activation_function):
 def flatten(input_data):
     """
     画像(2次元以上)を1次元配列に変換
-    (実際はバッチデータのデータ数も含めた3次元以上→2次元に変換される)
+    (実際はバッチデータのデータ数も含め、3次元以上→2次元に変換される)
     """
     original_shape = input_data.shape
     return input_data.reshape(original_shape[0], -1)
 
 def calc_flatten_shape(input_shape):
     """
-    flatten後のデータ形状を計算 (バッチデータのデータ数は次元に含めない)
+    flatten後のデータ形状を計算 (Kerasの仕様に合わせ、バッチデータのデータ数は次元に含めない)
     """
     reshaped_shape = (np.prod(input_shape), )
     return reshaped_shape
+
+def im2col():
+    pass
